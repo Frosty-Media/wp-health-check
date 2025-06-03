0101effffa61e77f5779e73bbcf37f559a2e2687
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-06-03 15:04:53 -0700

    Add Utility the our service provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace FrostyMedia\WpHealthCheck;
 
+use FrostyMedia\WpHealthCheck\HealthCheck\Utility;
 use Pimple\Container as PimpleContainer;
 use Pimple\ServiceProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,7 @@ class ServiceProvider implements ServiceProviderInterface
 {
 
     public const string REQUEST = 'request';
+    public const string UTILITY = 'utility';
 
     /**
      * Register services.
@@ -24,5 +26,11 @@ class ServiceProvider implements ServiceProviderInterface
     public function register(PimpleContainer $pimple): void
     {
         $pimple[self::REQUEST] = static fn(): Request => Request::createFromGlobals();
+        $pimple[self::UTILITY] = static function () use ($pimple): Utility {
+            $utility = new Utility();
+            $utility->setRequest($pimple[self::REQUEST]);
+            $utility->timerStart();
+            return $utility;
+        };
     }
 }
