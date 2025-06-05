f1c921c02dfe4ad83a9b45ea622a79320522f86b
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-06-05 14:07:17 -0700

    Update our MU plugin

--- mu-plugins/wp-health-check.php
+++ mu-plugins/wp-health-check.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Plugin Name: WP Health Check (MU)
+ * Plugin Name: WP Health Check
  * Plugin URI: https://github.com/Frosty-Media/wp-health-check
  * Description: A simple WordPress health check endpoint.
  * Version: 1.0.0
@@ -13,40 +13,12 @@ declare(strict_types=1);
 
 namespace FrostyMedia\WpHealthCheck;
 
-use TheFrosty\WpUtilities\Plugin\PluginFactory;
-use TheFrosty\WpUtilities\WpAdmin\DisablePluginUpdateCheck;
-use function define;
+use FrostyMedia\WpHealthCheck\RestApi\RestApi;
 use function defined;
-use function file_exists;
-use function is_plugin_active;
-use const WP_PLUGIN_DIR;
-use const WPMU_PLUGIN_DIR;
 
 defined('ABSPATH') || exit;
 
-require_once ABSPATH . 'wp-admin/includes/plugin.php';
-if (is_plugin_active('wp-health-check/wp-health-check.php')) {
-    include_once WP_PLUGIN_DIR . '/wp-health-check/wp-health-check.php';
-} elseif (file_exists(WPMU_PLUGIN_DIR . '/wp-health-check/wp-health-check.php')) {
-    include_once WPMU_PLUGIN_DIR . '/wp-health-check/wp-health-check.php';
-} else {
-    return;
-}
+add_action('init', static function (): void {
+    add_action('rest_api_init', [new RestApi(), 'initializeRoute']);
+});
 
-$plugin = PluginFactory::create('wp-health-check');
-$container = $plugin->getContainer();
-$container->register(new ServiceProvider());
-$plugin->add(new DisablePluginUpdateCheck())->initialize();
-
-/** @var \Symfony\Component\HttpFoundation\Request $request */
-$request = $container->get(ServiceProvider::REQUEST);
-/** @var HealthCheck\Utility $utility */
-$utility = $container->get(ServiceProvider::UTILITY);
-
-if (
-    $request->server->has('REQUEST_URI') &&
-    stripos($request->server->get('REQUEST_URI'), '/health-check') !== false
-) {
-    define('DISABLE_WP_CRON', true);
-    $utility->respond();
-}
