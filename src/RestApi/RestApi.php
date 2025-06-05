7b0fbb2dd636014ca2166e332c178b257de921fe
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-06-05 16:59:43 -0700

    Add "basic" schema to REST API

--- src/RestApi/RestApi.php
+++ src/RestApi/RestApi.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace FrostyMedia\WpHealthCheck\RestApi;
 
 use FrostyMedia\WpHealthCheck\HealthCheck\Utility;
-use Symfony\Component\HttpFoundation\Request;
 use WP_REST_Server;
 use function apply_filters;
 use function is_user_logged_in;
-use function microtime;
 use function register_rest_route;
-use function time;
 
 /**
  * Class RestApi
@@ -29,16 +26,14 @@ class RestApi
      */
     public function initializeRoute(): void
     {
-        $utility = new Utility();
-        $utility->setRequest(Request::createFromGlobals());
-        $utility->setTime(time())->setTimer(microtime(true));
         register_rest_route(
             self::NAMESPACE,
             self::ROUTE,
             [
-                'callback' => [$utility, 'respond'],
-                'methods'  => WP_REST_Server::READABLE,
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [new Utility(), 'respond'],
                 'permission_callback' => fn(): bool => $this->permissionCallback(),
+                'schema' => $this->getParameterArgs(),
             ]
         );
     }
@@ -52,5 +47,63 @@ class RestApi
     {
         $require_auth = apply_filters(self::HOOK_NAME_REQUIRE_REST_AUTHENTICATION, true);
         return !($require_auth === true) || is_user_logged_in();
+    }
+
+    /**
+     * @return array[]
+     */
+    private function getParameterArgs(): array
+    {
+        return [
+            Utility::PARAM_ERRORS => [
+                'type' => ['string', 'null'],
+                'description' => 'Health check error message(s).',
+            ],
+            Utility::PARAM_BUILD => [
+                'type' => ['array', 'null'],
+                'description' => 'Build details found in an .info file.',
+                'items' => [
+                    'enum' => ['commit', 'version'],
+                    'type' => 'string',
+                ],
+            ],
+            Utility::PARAM_MYSQL => [
+                'type' => ['array', 'null'],
+                'description' => 'MySQL information.',
+                'items' => [
+                    'enum' => ['errors', 'extension', 'instance', 'num_queries', 'status'],
+                    'type' => ['string', 'null'],
+                ],
+            ],
+            Utility::PARAM_PHP => [
+                'type' => ['array', 'null'],
+                'description' => 'PHP information.',
+                'items' => [
+                    'enum' => ['memory_limit', 'version'],
+                    'type' => 'string',
+                ],
+            ],
+            Utility::PARAM_REDIS => [
+                'type' => ['array', 'null'],
+                'description' => 'Object cache information.',
+                'items' => [
+                    'enum' => ['cache', 'client', 'flush', 'errors', 'hits', 'misses', 'status'],
+                    'type' => ['string', 'null'],
+                ],
+            ],
+            Utility::PARAM_STATUS => [
+                'type' => ['string'],
+                'enum' => [Utility::STATUS_OK, Utility::STATUS_FAILURE, Utility::STATUS_UNKNOWN, Utility::STATUS_WARN],
+                'description' => 'The WordPress application status.',
+            ],
+            Utility::PARAM_WP => [
+                'type' => ['array', 'null'],
+                'description' => 'WordPress information.',
+                'items' => [
+                    'type' => ['integer', 'string', 'null'],
+                    'enum' => ['wp_db_version', 'db_version', 'version', 'cli'],
+                ],
+            ],
+        ];
     }
 }
